3a23412df38481eaf263489ff8ec3553f72802a1
WuYuqian35 <79963059+WuYuqian35@users.noreply.github.com>
2021-03-11 16:21:29 +0800

    Update fibonacci.py

--- practical 5/fibonacci.py
+++ practical 5/fibonacci.py
@@ -2,8 +2,10 @@ a=1
 b=1
 c=0
 print(a,',',b)
+#print the first two numbers
+
 for i in range(1,12):
-	c=a+b
-	a=b
+	c=a+b              #calculate the third number
+	a=b                #every number moves forward
 	b=c
-	print(c,' ')
+	print(c,' ')       #print every answer we have calculated
