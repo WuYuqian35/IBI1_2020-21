380b0146c1bee897aabcfaddb400f976f5fe68aa
WuYuqian35 <79963059+WuYuqian35@users.noreply.github.com>
2021-03-18 15:09:51 +0800

    Update variables.py

--- practical 5/variables.py
+++ practical 5/variables.py
@@ -3,7 +3,7 @@ b=190784                                 #Young's birthday
 c=100321                                #today's date
 d=abs(a-c)                              #the absolute difference
 e=abs(b-c)
-if d>e:
+if d>e:                                 #compare the absolute value
   print('d is greater')
 else:
   print('c is greater')
@@ -11,10 +11,10 @@ else:
   
 x=False
 y=True
-z=(x and not y) or (y and not x)
+z=(x and not y) or (y and not x)       #define z
 print(z)
 
-w=x!=y
+w=x!=y                             #define w as true
 if w==z:
   print(w==z)
   
