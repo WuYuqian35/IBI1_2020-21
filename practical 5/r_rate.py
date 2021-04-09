1d0dac25ac3c3536d0b5ec845be41dc39e329a59
WuYuqian35 <79963059+WuYuqian35@users.noreply.github.com>
2021-04-09 15:50:51 +0800

    Update r_rate.py

--- practical 5/r_rate.py
+++ practical 5/r_rate.py
@@ -1,5 +1,5 @@
 n=84                                       #the initial number
-r=1.2                                      #the increasing rate
+r=1.1                                      #the increasing rate
 for i in range(1,6):
   n=n*r+n                                    #the infected people in every round
 
